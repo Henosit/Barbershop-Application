35378b13492143f7dff74da49c008faa52322a7d
Henosit <chenmoasis@gmail.com>
2023-09-13 18:29:03 +0300

    Added appointment activity and xml + appointment details java class, implemented booking to database. **next time- make sure that the time slot is available, etc..

--- app/src/main/java/com/example/barbershopapp/activities/AppointmentActivity.java
+++ app/src/main/java/com/example/barbershopapp/activities/AppointmentActivity.java
@@ -9,9 +9,18 @@ import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+
 import com.example.barbershopapp.R;
+import com.example.barbershopapp.utils.ReadWriteAppointmentDetails;
+import com.example.barbershopapp.utils.ReadWriteUserDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AppointmentActivity extends Activity {
 
@@ -70,11 +79,43 @@ public class AppointmentActivity extends Activity {
 
                 // Perform booking logic here (e.g., save to a database)
 
-                // Display a confirmation message
-                Toast.makeText(getApplicationContext(), "Appointment booked for " +
-                                selectedDate + "\nBarber: " + selectedBarber + "\nTreatment: " + selectedTreatment + "\nTime Slot: " + selectedTimeSlot,
-                        Toast.LENGTH_LONG).show();
+                bookAppointment(selectedDate,selectedBarber,selectedTreatment,selectedTimeSlot);
             }
         });
+    }
+
+    public void bookAppointment(String date, String barber, String treatment, String timeSlot) {
+        // TODO: Perform availability check here (query your database or data source)
+        if (isSlotAvailable(date, barber, treatment, timeSlot)) {
+            // Enter Appointment Data into the Firebase Realtime DB
+            ReadWriteAppointmentDetails writeAppointmentDetails = new ReadWriteAppointmentDetails(date, barber, treatment, timeSlot);
+
+            // Extracting user reference from DB for "Appointments"
+            DatabaseReference referenceAppointment = FirebaseDatabase.getInstance().getReference("Appointments");
+            referenceAppointment.child(firebaseUser.getUid()).setValue(writeAppointmentDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        // Display a confirmation message
+                        Toast.makeText(getApplicationContext(), "Appointment booked for " +
+                                        date + "\nBarber: " + barber + "\nTreatment: " + treatment + "\nTime Slot: " + timeSlot,
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Appointment booking failed. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            // Show a message to the user indicating that the selected slot is already booked
+            Toast.makeText(getApplicationContext(), "This time slot is already booked. Please choose another time slot.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Function to check if the selected date and time slot are available (you need to implement this)
+    private boolean isSlotAvailable(String date, String barber, String treatment, String timeSlot) {
+        // TODO: Implement logic to check availability from your data source
+        // Return true if the slot is available, false otherwise.
+        // You may need to query your database or perform other checks.
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/barbershopapp/utils/ReadWriteAppointmentDetails.java
@@ -0,0 +1,50 @@
+package com.example.barbershopapp.utils;
+
+import java.util.Date;
+
+public class ReadWriteAppointmentDetails {
+
+    private String date, barber, treatment, timeSlot;
+
+    public ReadWriteAppointmentDetails() {
+    }
+
+    public ReadWriteAppointmentDetails(String date, String barber, String treatment, String timeSlot) {
+        this.date = date;
+        this.barber = barber;
+        this.treatment = treatment;
+        this.timeSlot = timeSlot;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBarber() {
+        return barber;
+    }
+
+    public void setBarber(String barber) {
+        this.barber = barber;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+}
