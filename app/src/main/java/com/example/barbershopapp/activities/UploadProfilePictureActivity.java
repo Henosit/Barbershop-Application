bc644917cfaedbc15fc7d345a8d239867906426f
Henosit <chenmoasis@gmail.com>
2023-10-29 14:55:18 +0200

    Update Profile Pic
    added code to this activity.
    updated gradle files.
    choosing a picture works.
    TODO fix problem- uploading the picture doesn't work in the firebase storage!

--- app/src/main/java/com/example/barbershopapp/activities/UploadProfilePictureActivity.java
+++ app/src/main/java/com/example/barbershopapp/activities/UploadProfilePictureActivity.java
@@ -1,37 +1,132 @@
 package com.example.barbershopapp.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.example.barbershopapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class UploadProfilePictureActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private ImageView imageViewPfpUpload;
     private FirebaseAuth authProfile;
     private StorageReference storageReference;
+    private FirebaseUser firebaseUser;
 
+    // For image selection
+    private ActivityResultLauncher<Intent> imageChooserLauncher;
+    private Uri uriImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_profile_picture);
-        getSupportActionBar().setTitle("Upload your Profile Picture");
-        Button buttonUploadPictureChoose= findViewById(R.id.upload_pic_choose_button);
-        Button buttonUploadPic=findViewById(R.id.upload_pic_button);
-        imageViewPfpUpload=findViewById(R.id.imageView_profile_dp);
-        authProfile=FirebaseAuth.getInstance();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        Button buttonUploadPictureChoose = findViewById(R.id.upload_pic_choose_button);
+        Button buttonUploadPic = findViewById(R.id.upload_pic_button);
+        imageViewPfpUpload = findViewById(R.id.imageView_profile_dp);
+        progressBar = findViewById(R.id.progressBar);
 
+        authProfile = FirebaseAuth.getInstance();
+        firebaseUser = authProfile.getCurrentUser();
+        storageReference = FirebaseStorage.getInstance("gs://barbershop-app-80e99.appspot.com").getReference();
+        Uri uri = firebaseUser.getPhotoUrl();
+
+        // Set User's current DP in ImageView (if uploaded already). We"ll use Picasso since ImageViewer SetImage doesn't work on Regular URIs.
+        Picasso.get().load(uri).into(imageViewPfpUpload);
+
+        // Initialize the ActivityResultLauncher for image selection
+        imageChooserLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null && result.getData().getData() != null) {
+                        Uri selectedImageUri = result.getData().getData();
+                        // Handle the selected image URI as needed
+                        imageViewPfpUpload.setImageURI(selectedImageUri);
+                    }
+                });
+
+        buttonUploadPictureChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+
+        buttonUploadPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                UploadPic();
+            }
+        });
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Use the ActivityResultLauncher to start the image selection activity
+        imageChooserLauncher.launch(intent);
+    }
+
+    private void UploadPic() {
+        if (uriImage != null) {
+            // Save the image with uid of the currently logged user
+            StorageReference fileReference = storageReference.child(authProfile.getCurrentUser().getUid() + "."
+            + getFileExtension(uriImage));
+
+            // Upload image to Storage
+            fileReference.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Uri downloadUri = uri;
+                            firebaseUser = authProfile.getCurrentUser();
+
+                            // Finally, set the display image of the user after upload
+                            UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(downloadUri).build();
+                            firebaseUser.updateProfile(profileChangeRequest);
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    // Obtain File Extension of the image
+    private String getFileExtension(Uri uri) {
+        // A content provider manages access to a central repository of the data.
+        // When you want to access data in a content provider, you use the ContentResolver object
+        // in your application's Context to communicate with the provider as a client.
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        // Return the extension
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 }
